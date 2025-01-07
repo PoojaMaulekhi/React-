83197a70ef613831bc354c72794b8a42e0703b7f
PoojaMaulekhi <poojamaulekhi7@gmail.com>
2025-01-07 14:53:07 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+// Import React library
+import React from 'react';
+// Import the WeatherApp component from the local file
+import WeatherApp from "./WeatherApp";
+
+// Define  main App  component
+export default function App() {
+  // Render  WeatherApp component 
+  return (
+    <>
+      <WeatherApp /> 
+    </>
+  );
+}
